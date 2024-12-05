069b27c58ff0b3ff5b235e3dddd65e3f3da1e762
gereltbaatar <mglgerelt@gmail.com>
2024-12-05 20:47:37 +0800

    admin deer folder Structure hiij mon adminPage bolon dashboardPage hiisen

---
+++ admin/components/pages/AdminPage.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+  return (
+    <main>
+      <div className="container m-auto">admin page</div>
+    </main>
+  );
+};
+
+export default AdminPage;
---
+++ admin/components/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+const DashboardPage = () => {
+  return (
+    <main>
+      <div className="container m-auto">dashboard page</div>
+    </main>
+  );
+};
+export default DashboardPage;
